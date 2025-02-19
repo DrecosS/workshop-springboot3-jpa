c9226cc4fa29ff0cb36243ea93e6a30478a81516
André S <snauriz_a@hotmail.com>
2025-02-18 23:37:39 -0300

    Service layer, component registration

--- src/main/java/com/praticandojava/course/repositories/UseRepository.java
+++ src/main/java/com/praticandojava/course/repositories/UseRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.praticandojava.course.entities.User;
 
+
 public interface UseRepository extends JpaRepository<User, Long> {
 	
 }
